f93df3c75f80e0f54e5a9ed5de8b9d9544a7e617
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-18 18:37:56 -0300

    Create Ex02HoraDoSistema.java

---
+++ Ex02HoraDoSistema.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Ex02HoraDoSistema {
+    
+public static void main(String[] args) {
+    
+
+    Date data = new Date();
+    System.out.println("A hora do sistema é: ");
+    System.out.println(data.toString());
+}
+}
