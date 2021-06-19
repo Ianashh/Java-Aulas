3d9c3ed2bb676ea7998caf77de18769c7327a2c4
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-19 00:33:01 -0300

    Create Ex03FunctionSum.java

---
+++ Ex03FunctionSum.java
@@ -0,0 +1,15 @@
+
+public class Ex03FunctionSum {
+
+public static void main(String[] args) {
+    sum(7,5);
+    sum(3,12);
+    sum(5,55);
+    sum(57,43);
+}
+static void sum (int number1, int number2) {
+
+    System.out.println(number1+number2);
+
+    }
+}
