b2d25e2706fb082b243dff855f58eb83ea5fc3f9
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-21 13:47:28 -0300

    Update Ex04TheSimpleCalculator.java

--- Ex04TheSimpleCalculator.java
+++ Ex04TheSimpleCalculator.java
@@ -15,57 +15,55 @@ System.out.printf("Olá Mundo!!%nEste é um novo programa de cálculo simples qu
     System.out.print("If you want continue in English, please, press 1 \n\nSe prefere continuar em português, por favor, digite 2: ");
     languages = keyboard.nextInt();
     
-    if (languages == 1){
-        System.out.print("Press the first number to calculate: ");
-        n1  = keyboard.nextInt();
-        System.out.print("Press the second number to calculate: ");
-        n2  = keyboard.nextInt();
-        System.out.print("[+] For Sum Press +\n[-] For Minus Press -\n[*] For Multiply Press *\n[/] For Divide Press /\n[f] For finish this program press f \n---> : ");
-        calc = keyboard.next();
-        System.out.println(calc);
-        System.out.println(n1);
-        System.out.println(n2);
+        if (languages == 1){
+            System.out.print("Press the first number to calculate: ");
+            n1  = keyboard.nextInt();
+            System.out.print("Press the second number to calculate: ");
+            n2  = keyboard.nextInt();
+            System.out.print("[+] For Sum Press +\n[-] For Minus Press -\n[*] For Multiply Press *\n[/] For Divide Press /\n[f] For finish this program press f \n---> : ");
+            calc = keyboard.next();
+            
+            }
         
-            if (calc == "+"){
-            System.out.println("\nThe sum of number "+ n1 +" with number" + n2 + " is equal to "+ (n1+n2) +" !!");
+            if (calc.equals("+")){
+            System.out.println("\nThe sum of number "+ n1 +" with number " + n2 + " is equal to "+ (n1+n2) +" !!\n");
             }
-            else if (calc == "-"){
-            System.out.println("\nThe subtraction of number "+ n1 +" by number" + n2 + " is equal to "+ (n1-n2) +" !!");
+            else if (calc.equals("-")){
+            System.out.println("\nThe subtraction of number "+ n1 +" by number " + n2 + " is equal to "+ (n1-n2) +" !!\n");
             }
-            else if (calc == "*"){
-                System.out.println("\nThe multiply of number "+ n1 +" by number" + n2 + " is equal to "+ (n1*n2) +" !!");
+            else if (calc.equals("*")){
+                System.out.println("\nThe multiply of number "+ n1 +" by number " + n2 + " is equal to "+ (n1*n2) +" !!\n");
             }
-            else if (calc == "/"){
-            System.out.println("\nThe division of number "+ n1 +" by number" + n2 + " is equal to "+ (n1/n2) +" !!");
+            else if (calc.equals("/")){
+            System.out.println("\nThe division of number "+ n1 +" by number " + n2 + " is equal to "+ (n1/n2) +" !!\n");
             }
-            else if ((calc == "f") || (calc == "F")){
-                System.out.print("The program has benn finalized!!");
+            else if ((calc.equals("f")) || (calc.equals("F"))){
+                System.out.print("The program has benn finalized!!\n");
             }
-        }
-    if (languages == 2){
-        System.out.print("Digite o primeiro número: ");
-        n1  = keyboard.nextInt();
-        System.out.print("Digite o primeiro número: ");
-        n2  = keyboard.nextInt();
-        System.out.print("[+] Para Somar digite +\n[-] Para Subtrair digite -\n[*] Para Multiplicar digite *\n[/] Para dividir digite /\n[f] Para Finalizar o programa digite f \n---> :");
-        calc = keyboard.next();        
         
-            if (calc == "+"){
-            System.out.println("\nA soma dos números "+ n1 +" e " + n2 + " é igual a "+ (n1+n2) +" !!");
-            }
-            else if (calc == "-"){
-            System.out.println("\nA subtração do número "+ n1 +" por " + n2 + " é igual a "+ (n1-n2) +" !!");
-            }
-            else if (calc == "*"){
-                System.out.println("\nA multiplicação do número "+ n1 +" por " + n2 + " é igual a "+ (n1*n2) +" !!");
-            }
-            else if (calc == "/"){
-            System.out.println("\nA divisão do número "+ n1 +" por " + n2 + " é igual a "+ (n1/n2) +" !!");
-            }
-            else if ((calc == "f") || (calc == "F")) {
+        else if (languages == 2){
+            System.out.print("Digite o primeiro número: ");
+            n1  = keyboard.nextInt();
+            System.out.print("Digite o primeiro número: ");
+            n2  = keyboard.nextInt();
+            System.out.print("[+] Para Somar digite +\n[-] Para Subtrair digite -\n[*] Para Multiplicar digite *\n[/] Para dividir digite /\n[f] Para Finalizar o programa digite f \n---> :");
+            calc = keyboard.next();        
+            if (calc.equals("+")){
+                System.out.println("\nA soma dos números "+ n1 +" e " + n2 + " é igual a "+ (n1+n2) +" !!\n");
+                }
+            else if (calc.equals("-")){
+                System.out.println("\nA subtração do número "+ n1 +" por " + n2 + " é igual a "+ (n1-n2) +" !!\n");
+                }
+            else if (calc.equals("*")){
+                System.out.println("\nA multiplicação do número "+ n1 +" por " + n2 + " é igual a "+ (n1*n2) +" !!\n");
+                }
+            else if (calc.equals("/")){
+                System.out.println("\nA divisão do número "+ n1 +" por " + n2 + " é igual a "+ (n1/n2) +" !!\n");
+                }
+            else if ((calc.equals("f")) || (calc.equals("F"))) {
                 System.out.print("O programa foi finalizado!!");
-            }
-        }
+                }
+                }
     
 keyboard.close();    
 }
