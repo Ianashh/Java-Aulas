327ab9808e26cc56e008672118408022eae563b9
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-20 23:36:26 -0300

    Update Ex03TheSimpleFunctionSum.java

--- Ex03TheSimpleFunctionSum.java
+++ Ex03TheSimpleFunctionSum.java
@@ -1,14 +1,20 @@
 
-public class Ex03FunctionSum {
+public class Ex03TheSimpleFunctionSum {
 
 public static void main(String[] args) {
     sum(7,5);
     sum(3,12);
     sum(5,55);
     sum(57,43);
+    sum2(7,5);
+    sum2(3,12);
+    sum2(5,55);
+    sum2(57,43);
 }
 static void sum (int number1, int number2) {
-
-    System.out.println("\nA soma dos numeros "+ number1 +" e " + number2 + " é igual a "+ (number1+number2) +" !!");
+    System.out.println("\nThe sum of "+ number1 +" with " + number2 + " is equal to "+ (number1+number2) +" !!");
+    }
+static void sum2 (int number1, int number2) {
+    System.out.println("\nA soma dos números "+ number1 +" e " + number2 + " é igual a "+ (number1+number2) +" !!");
     }
 }
