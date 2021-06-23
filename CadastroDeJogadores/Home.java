b0fc6a7b5b359819668f57b58182ee8b471a52ec
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-23 01:41:31 -0300

    clearcode

--- CadastroDeJogadores/Home.java
+++ CadastroDeJogadores/Home.java
@@ -1,15 +1,15 @@
 package CadastroDeJogadores;
-import java.util.Scanner;
+
 
 public class Home {
     public static void main (String[] args){
-        Scanner input = new Scanner(System.in);
+        
         Jogador jogador1 = new Jogador();
         jogador1.firstname="";
         jogador1.lastname="";
         jogador1.shirt=0;
         
 
-input.close();
+
 }
 }
--- CadastroDeJogadores/Jogador.java
+++ CadastroDeJogadores/Jogador.java
@@ -11,17 +11,17 @@ public class Jogador {
     int n;
     public Jogador(){
     Scanner input = new Scanner(System.in);
-    System.out.println("Quantos jogadores deseja criar: ");
+    System.out.print("Quantos jogadores deseja criar: ");
     jogadores = input.nextInt();
 
-    for (n=1; n <= jogadores+1 ; n++){
+    for (n=1; n < jogadores+1 ; n++){
     System.out.printf("\nCadastre o nome do jogador %d: ",n);
     firstname = input.next();
     System.out.printf("\nCadastre o último nome do jogador %d: ",n);
     lastname = input.next();
     System.out.printf("\nCadastre o número da camiseta do jogador %d: ",n);
     shirt = input.nextInt();
-    System.out.printf("\n\nO jogador %s %s foi criado com sucesso!!\nSua camisa é a número %d", firstname,lastname,shirt);
+    System.out.printf("\n\nO jogador %s %s foi criado com sucesso!!\nSua camisa é a número %d\n\n", firstname,lastname,shirt);
     }
     input.close();
 }
