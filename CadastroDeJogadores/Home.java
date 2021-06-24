b81f8de9d00bc9a563851416a9d0877e7d66737c
Ianashh <82633785+Ianashh@users.noreply.github.com>
2021-06-24 02:59:35 -0300

    Quem não sofre não aprende haha
    Abri uma branch pra testar algumas coisas novas como o conceito de .add semelhante ao .append do Python. No entanto, o método construtor Jogador( ) até recebe e add os nomes aos Arrays, porém quando chamo a variável fora do loop for do método, eu constato que ela esta vazia, ou seja, o Array  acaba existindo apenas dentro do método e é apagado ao fim... "bloco"?? hahahha ao fim do " } " o array deixa de existir. Contudo, a parte boa é que aprendi a construir e add coisas nos arrays lendo a documentação que se mostrou primordial para a evolução de hoje. Talvez esses commits se tornem meu diário kkkkkkkk Labs é isso hahahaha

--- CadastroDeJogadores/Home.java
+++ CadastroDeJogadores/Home.java
@@ -1,12 +1,14 @@
 package CadastroDeJogadores;
 
+import java.util.ArrayList;
 
 public class Home {
     public static void main (String[] args){
         Jogador jogador1 = new Jogador();
-        jogador1.firstname="";
-        jogador1.lastname="";
+        jogador1.firstname = new ArrayList<>();
+        jogador1.lastname= new ArrayList<>();
         jogador1.shirt=0;
+        jogador1.status();
         
 
 
--- CadastroDeJogadores/Jogador.java
+++ CadastroDeJogadores/Jogador.java
@@ -1,28 +1,44 @@
 package CadastroDeJogadores;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 
 
 public class Jogador {
-    String firstname; 
-    String lastname;
-    int shirt;
-    int jogadores;
-    int n;
+    public int n;
+    public int c;
+    public List<String> firstname = new ArrayList<>(); 
+    public List<String> lastname = new ArrayList<>();
+    public int shirt;
+    public int jogadores;
+    private String Liga = "UEFA Champions League";
+    private String Sexo = "Masculino";
+
     public Jogador(){
     Scanner input = new Scanner(System.in);
     System.out.print("Quantos jogadores deseja criar: ");
     jogadores = input.nextInt();
 
     for (n=1; n < jogadores+1 ; n++){
-    System.out.printf("\nCadastre o nome do jogador %d: ",n);
-    firstname = input.next().trim().toUpperCase();
-    System.out.printf("\nCadastre o último nome do jogador %d: ",n);
-    lastname = input.next().trim().toUpperCase();
-    System.out.printf("\nCadastre o número da camiseta do jogador %d: ",n);
+    System.out.printf("\nCadastre o nome do jogador 0%d: ",n);
+    firstname.add(input.next().trim().toUpperCase());
+    System.out.printf("\nCadastre o último nome do jogador 0%d: ",n);
+    lastname.add(input.next().trim().toUpperCase());
+    lastname.addAll(Arrays.asList());
+    System.out.printf("\nCadastre o número da camisa do jogador 0%d: ",n);
     shirt = input.nextInt();
-    System.out.printf("\n\nO jogador %s %s foi criado com sucesso!!\nSua camisa é a número %d\n\n", firstname,lastname,shirt);
+    System.out.printf("\n\nO jogador %s %s foi criado com sucesso!!\nSua camisa é a número %d.\n\n", firstname.get(n-1),lastname.get(n-1),shirt);
     }
     input.close();
+    }
+    public void status(){
+        for (c=0; c < jogadores ; c++){
+        System.out.printf("\nNome do jogador: %s %s", firstname.get(c), this.lastname);
+        System.out.printf("\nNúmero da camisa do jogador: %d", this.shirt);
+        System.out.printf("\nNome da Liga do jogador: %s", this.Liga);
+        System.out.printf("\nLiga do sexo: %s", this.Sexo);
+        }
 }
 }
